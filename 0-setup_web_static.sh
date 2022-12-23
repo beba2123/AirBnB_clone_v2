e99c3ba8949e42dfe0021c3650a7d9b6b164abbb
beba2123 <100466939+beba2123@users.noreply.github.com>
2022-12-22 21:22:43 -0800

    submitted

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,27 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
-
+# sets up your web servers for the deployment of web_static
 apt-get update
 apt-get -y install nginx
-# Install Nginx if it not already installed
-mkdir -p /data/web_static/releases/test/
-# Create the folder /data/ if it doesn’t already exist
-# Create the folder /data/web_static/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
-# Create the folder /data/web_static/shared/ if it doesn’t already exist
-echo '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' > /data/web_static/releases/test/index.html
-# Create a fake HTML file /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Create a symbolic link /data/web_static/current linked to the...
-# .../data/web_static/releases/test/ folder. If the symbolic link already...
-# ...exists, it should be deleted and recreated every time the script is ran.
-chown -hR ubuntu:ubuntu /data/
-# Give ownership of the /data/ folder to the ubuntu user AND group (you can
-# assume this user and group exist). This should be recursive; everything
-# inside should be created/owned by this user/group.
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-# Update the Nginx configuration to serve the content of
-# /data/web_static/current/ to hbnb_static
-# (ex: https://mydomainname.tech/hbnb_static).
-# Don’t forget to restart Nginx after updating the configuration:
+mkdir -p /data/web_static/releases/test/
+echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+
+if [[ -L /data/web_static/current ]]
+then
+    rm /data/web_static/current
+fi
+
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+
+if grep -q hbnb_static /etc/nginx/sites-available/default
+then
+    echo ""
+else
+    sed -i '/:80 default_server/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+fi
+
 service nginx restart
