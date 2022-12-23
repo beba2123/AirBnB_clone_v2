b9aca6674ccc3c742dfdf9d59240d38c774e044b
beba2123 <100466939+beba2123@users.noreply.github.com>
2022-12-22 21:49:43 -0800

    submitted with updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,44 @@
 #!/usr/bin/env bash
-# Sets up web servers for deployment
-FILE=/etc/nginx/sites-available/default
-STRING="location /hbnb_static/ {\nalias /data/web_static/current/; \n}\n"
-HTML="<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>"
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases/test
-echo -e "$HTML" > /data/web_static/releases/test/index.html
+# Prepare my webservers (web-01 & web-02)
+
+# uncomment for easy debugging
+#set -x
+
+# colors
+blue='\e[1;34m'
+#brown='\e[0;33m'
+green='\e[1;32m'
+reset='\033[0m'
+
+echo -e "${blue}Updating and doing some minor checks...${reset}\n"
+
+# install nginx if not present
+if [ ! -x /usr/sbin/nginx ]; then
+	sudo apt-get update -y -qq && \
+	     sudo apt-get install -y nginx
+fi
+
+echo -e "\n${blue}Setting up some minor stuff.${reset}\n"
+
+# Create directories...
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
+
+# create index.html for test directory
+echo "<h1>Welcome to th3gr00t.tech <\h1>" | sudo dd status=none of=/data/web_static/releases/test/index.html
+
+# create symbolic link
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+# give user ownership to directory
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "39i $STRING" $FILE
+
+# backup default server config file
+sudo cp /etc/nginx/sites-enabled/default nginx-sites-enabled_default.backup
+
+# Set-up the content of /data/web_static/current/ to redirect
+# to domain.tech/hbnb_static
+sudo sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
 sudo service nginx restart
+
+echo -e "${green}Completed${reset}"
